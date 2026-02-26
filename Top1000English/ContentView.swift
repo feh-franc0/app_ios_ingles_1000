bdd95a594bcd49e40461ffb8262b0830e0782e3d
feh-franc0 <fernandofv1110@gmail.com>
2026-02-25 23:28:35 -0300

    Initial Commit

---
+++ Top1000English/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Top1000English
+//
+//  Created by Fernando Franco on 25/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Top1000English/Top1000EnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  Top1000EnglishApp.swift
+//  Top1000English
+//
+//  Created by Fernando Franco on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Top1000EnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
